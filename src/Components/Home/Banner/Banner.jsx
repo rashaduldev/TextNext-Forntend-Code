90780da64a49f6a1c3a150625cf9a5c40b1d77bc
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2024-02-09 12:23:34 +0600

    suitableBanner and search input added

---
+++ src/Components/Home/Banner/Banner.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+const Banner = ({ onSearch }) => {
+const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+
+  return (
+    <div className="text-center items-center h-[60vh] flex justify-center">
+      <div className="relative mx-10">
+        <h1 className="text-3xl font-bold my-8 lg:text-5xl">
+        Search based on the users name.
+        </h1>
+        <input
+          className="p-2 mt-8 w-full lg:w-3/4 rounded"
+          style={{ border: "1px solid black" }}
+          type="text"
+          placeholder="Search here...."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className="absolute top-[169px] right-[1px] md:top-[133px] lg:top-[145px] lg:right-[93px]">
+          <button
+            style={{ borderRight: "1px solid black" }}
+            className="text-white bg-red-500 p-2 rounded-r-sm"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
